96da593fdf5f025934f5eb194afea2af7054cdb7
rorturtola <transfreddy@hotmail.com>
2015-11-09 14:30:28 +0000

    update recipe pages

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,4 +4,48 @@ class RecipesController < ApplicationController
        @recipes = Recipe.all
        
    end
+   
+   def show
+      @recipe = Recipe.find(params[:id])
+   end
+   
+   def new
+      @recipe =  Recipe.new
+   end
+   
+   def create
+      @recipe = Recipe.new(recipe_params)
+      @recipe.chef = Chef.find(2)
+      
+      if @recipe.save
+         #do somethng
+         flash[:success] = " Your recipe was created successfully!"
+         redirect_to recipes_path
+         
+      else
+         render :new
+      end
+   end 
+   
+   def edit
+      @recipe = Recipe.find(params[:id])
+   end
+   
+   def update
+      @recipe = Recipe.find(params[:id])
+      if @recipe.update(recipe_params)
+         #do something
+         flash[:success] = "Your recipe was updated successfuly!"
+         redirect_to recipe_path(@recipe)
+         
+      else
+         render :edit
+      end
+   end
+   
+   private
+   
+   def recipe_params
+      params.require(:recipe).permit(:name, :summary, :description)
+   end
 end
