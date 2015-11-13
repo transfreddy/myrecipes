ec9f1eefbe7d9cae30c2115df4ce4b94bca55623
rorturtola <transfreddy@hotmail.com>
2015-11-13 14:45:14 +0000

    add image uploader

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,5 +4,15 @@ class Recipe < ActiveRecord::Base
     validates :name, presence: true, length:{ maximum: 100, too_long: "%{count} characters is more than maximum allowed", minimum: 5}
     validates :summary, presence: true, length:{ maximum: 100, too_long: "%{count} characters is more than maximum allowed", minimum: 10}
     validates :description, presence: true, length:{ maximum: 500, too_long: "%{count} characters is more than maximum allowed", minimum: 15}
+    mount_uploader :picture, PictureUploader
+    validate :picture_size
+    
+    private
+    def picture_size
+      if  picture.size > 5.megabytes
+          error.add(:picture, "should be less than 5MB")
+      end
+    end
+    
     
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151104084921) do
+ActiveRecord::Schema.define(version: 20151113133403) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "email"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20151104084921) do
     t.integer  "chef_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "picture"
   end
 
 end
