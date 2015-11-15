11b03e2fe52bea8306aeec6b0c219e2df28b1376
rorturtola <transfreddy@hotmail.com>
2015-11-15 11:59:56 +0000

    Create chef resources and add authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,22 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
       
-
+  helper_method :current_user, :logged_in?
+  
+ def current_user
+   @current_user ||= Chef.find(session[:chef_id]) if session[:chef_id]
+ end
+ 
+ def logged_in?
+   !!current_user
+ end
+ 
+ def require_user
+     if !logged_in?
+         flash[:danger] = "Your must be logged in to perform action"
+         redirect_to recipes_path
+     end
+ end
+ 
+ 
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,7 @@
 class RecipesController < ApplicationController
+   before_action :set_recipe, only: [:edit, :update, :show, :like]
+   before_action :require_user, except:[:show, :index]
+   before_action :require_same_user, only: [:edit, :update]
 
    def index
        @recipes = Recipe.paginate(page: params[:page], per_page: 4)
@@ -6,7 +9,7 @@ class RecipesController < ApplicationController
    end
 
    def show
-      @recipe = Recipe.find(params[:id])
+      
    end
 
    def new
@@ -15,7 +18,7 @@ class RecipesController < ApplicationController
 
    def create
       @recipe = Recipe.new(recipe_params)
-      @recipe.chef = Chef.find(2)
+      @recipe.chef = current_user
 
       if @recipe.save
          #do somethng
@@ -28,11 +31,11 @@ class RecipesController < ApplicationController
    end
 
    def edit
-      @recipe = Recipe.find(params[:id])
+      
    end
 
    def update
-      @recipe = Recipe.find(params[:id])
+      
       if @recipe.update(recipe_params)
          #do something
          flash[:success] = "Your recipe was updated successfuly!"
@@ -44,8 +47,8 @@ class RecipesController < ApplicationController
    end
 
    def like
-      @recipe = Recipe.find(params[:id])
-      like = Like.create(like: params[:like], chef: Chef.first, recipe: @recipe)
+      
+      like = Like.create(like: params[:like], chef: current_usert, recipe: @recipe)
    if like.valid?
       flash[:success] = "Your selection was successful"
       redirect_to :back
@@ -60,4 +63,16 @@ class RecipesController < ApplicationController
    def recipe_params
       params.require(:recipe).permit(:name, :summary, :description, :picture)
    end
+   
+   def set_recipe
+      @recipe = Recipe.find(params[:id])
+   end
+   
+   def require_same_user
+      if current_user != @recipe.chef
+       flash[:danger] = "You can only edit your own recipes"
+       redirect_to recipes_path
+      end
+   end
+   
 end
