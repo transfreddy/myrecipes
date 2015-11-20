822cb24cf45c1d400523437c08191b7a1b373d29
rorturtola <transfreddy@hotmail.com>
2015-11-20 06:36:10 +0000

    edit 9

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -2,10 +2,10 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider              => 'AWS',
-    :aws_access_key_id     => ENV['S3_KEY'],
-    :aws_secret_access_key => ENV['S3_SECRET']
+    :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+    :aws_secret_access_key => ENV['S3_SECRET_KEY']
   }
 
-  config.fog_directory =  ENV['S3_BUCKET_NAME']
+  config.fog_directory =  ENV['S3_BUCKET']
   config.cache_dir     = "#{Rails.root}/tmp/uploads"   # For Heroku
 end
