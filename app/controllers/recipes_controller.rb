7392b4dc40c89747eded8eec5e0bfc621584c163
rorturtola <transfreddy@hotmail.com>
2015-11-21 09:24:54 +0000

    Add admin functionality

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,6 +3,7 @@ class RecipesController < ApplicationController
    before_action :require_user, except:[:show, :index, :like]
    before_action :require_user_like, only: [:like]
    before_action :require_same_user, only: [:edit, :update]
+   before_action :admin_user, only: :destroy
 
    def index
        @recipes = Recipe.paginate(page: params[:page], per_page: 4)
@@ -50,13 +51,19 @@ class RecipesController < ApplicationController
    def like
       
       like = Like.create(like: params[:like], chef: current_usert, recipe: @recipe)
-   if like.valid?
+    if like.valid?
       flash[:success] = "Your selection was successful"
       redirect_to :back
    else
       flash[:danger] = "You can only like/dislike one recipe once"
       redirect_to :back
+    end
    end
+   
+   def destroy
+     Recipe.find(params[:id]).destroy
+     flash[:success] = "Recipe Deleted"
+     redirect_to recipes_path
    end
 
    private
@@ -70,7 +77,7 @@ class RecipesController < ApplicationController
    end
    
    def require_same_user
-      if current_user != @recipe.chef
+      if current_user != @recipe.chef and !current_user.admin?
        flash[:danger] = "You can only edit your own recipes"
        redirect_to recipes_path
       end
@@ -81,6 +88,10 @@ class RecipesController < ApplicationController
          flash[:danger] = "Your must be logged in to perform action"
          redirect_to :back
      end
+   end
+   
+   def admin_user
+      redirect_to recipes_path unless current_user.admin?
    end
  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151120083012) do
+ActiveRecord::Schema.define(version: 20151121083608) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "chefname"
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
   create_table "ingredients", force: :cascade do |t|
